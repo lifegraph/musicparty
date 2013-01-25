0c0c530e89454c7e1a7e929eae8486317e06f62c
johnnyman727 <jonathan.mckay@students.olin.edu>
2013-01-24 22:43:57 -0500

    Added latest song playing after tag

--- local_app.js
+++ local_app.js
@@ -1,5 +1,5 @@
 var serialport = require("serialport");
-var trackRecord = require("./trackrecord");
+var trackRecord = require("./trackrecord/trackrecord");
 var SerialPort = serialport.SerialPort;
 // var arduino_port = "/dev/tty.usbmodem1411";
 var arduino_port = "/dev/cu.usbmodemfa131";
@@ -7,27 +7,31 @@ var serialPort = new SerialPort(arduino_port, {
   parser: serialport.parsers.readline("\n") 
 });
 
+
 // hard-coded for now, but should go up to a server to grab corresponding fbid
 
 var my_uid = '0x2E 0x38 0x53 0xA5'; 
 
 console.log("Hi! listening to " + arduino_port);
-serialPort.on("data", function (data) {
-  console.log("here: "+data);
+trackRecord.initialize(function(err, facebookAPI) {
+  serialPort.on("data", function (data) {
+    console.log("here: "+data);
 
-  // The prefix we set before the uid on the arduino end of things
-  var prefix = "  UID Value: "; // The prefix before the data we care about comes through
+    // The prefix we set before the uid on the arduino end of things
+    var prefix = "  UID Value: "; // The prefix before the data we care about comes through
 
-  // If we have a uid calue
-  if (data.indexOf(prefix) == 0) {
+    // If we have a uid calue
+    if (data.indexOf(prefix) == 0) {
 
-    // Grab the uid
-    uid = data.substring(prefix.length).trim();
-    console.log("WHOA!");
-    console.log(uid);
-    if (uid == my_uid) {
-      console.log("GO TIME")
-
+      // Grab the uid
+      uid = data.substring(prefix.length).trim();
+      console.log("WHOA!");
+      console.log(uid);
+      if (uid == my_uid) {
+        console.log("GO TIME");
+        console.log(facebookAPI);
+        trackRecord.playFavorites(facebookAPI, "jon.mckay");
+      }
     }
-  }
+  });
 });
