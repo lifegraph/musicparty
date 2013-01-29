98f4f17035ba85e2e3001674d8256548aac937b0
johnnyman727 <jonathan.mckay@students.olin.edu>
2013-01-29 16:12:22 -0500

    Now converts JSON from the server to spotify tracks and plays it

--- local_app.js
+++ local_app.js
@@ -4,9 +4,9 @@ var gateKeeper = require("./gate-keeperClient.js");
 var SerialPort = serialport.SerialPort;
 // var arduino_port = "/dev/tty.usbmodem1411";
 var arduino_port = "/dev/cu.usbmodemfa131";
-var serialPort = new SerialPort(arduino_port, { 
-  parser: serialport.parsers.readline("\n") 
-});
+// var serialPort = new SerialPort(arduino_port, { 
+//   parser: serialport.parsers.readline("\n") 
+// });
 
 
 var trying_to_connect_uid = null; // the fob id that we are trying to connect to
@@ -20,46 +20,44 @@ var fakeUID = "0x010x010x010x01"
 
 console.log("Hi! listening to " + arduino_port);
 
-// Initialize our spotify streaming
-// trackRecord.initialize(function(err, facebookAPI) {
-  // After initialized, when we get a tag from the RF Reader
-  serialPort.on("data", function (data) {
-    // Print out the tag data
-    console.log("ID Data received: : "+ data);
 
-    // The prefix we set before the uid on the arduino end of things
-    var prefix = "  UID Value: "; // The prefix before the data we care about comes through
+// After initialized, when we get a tag from the RF Reader
+// serialPort.on("data", function (data) {
+//   // Print out the tag data
+//   console.log("ID Data received: : "+ data);
 
-    // If we have a uid calue
-    if (data.indexOf(prefix) == 0) {
+//   // The prefix we set before the uid on the arduino end of things
+//   var prefix = "  UID Value: "; // The prefix before the data we care about comes through
 
-      // Grab the uid
-      trying_to_connect_uid = data.substring(prefix.length).trim();
+//   // If we have a uid calue
+//   if (data.indexOf(prefix) == 0) {
 
-      // If the last uid is still stored (it's stored for 1 seconds) 
-      // and the same uid is tagged, don't register it. This is just to
-      // prevent repeated taggings. 
-      if (trying_to_connect_uid == last_uid_to_connect) {
-        console.log("This is a re-tag. No changes will occur.");
-        set_last_uid_to_connect(trying_to_connect_uid);
-        return;
-      }
+//     // Grab the uid
+//     trying_to_connect_uid = data.substring(prefix.length).trim();
 
-      gateKeeper.authenticate(trying_to_connect_uid, function(error, facebookID) {
+//     // If the last uid is still stored (it's stored for 1 seconds) 
+//     // and the same uid is tagged, don't register it. This is just to
+//     // prevent repeated taggings. 
+//     if (trying_to_connect_uid == last_uid_to_connect) {
+//       console.log("This is a re-tag. No changes will occur.");
+//       set_last_uid_to_connect(trying_to_connect_uid);
+//       return;
+//     }
 
-        if (facebookID) {
-          console.log("VALID TAG: IT'S GO TIME FOR USERNAME: " + facebookID);
-          trackRecord.playFavorites(facebookID);
-          console.log("Streaming: " + trackRecord.isStreaming);
-          set_last_uid_to_connect(trying_to_connect_uid);
-        }
-        else if(error) {
-          console.log("Error with auth: " + error.message);
-          return;
-        } 
-      });
-    }
-  });
+//     gateKeeper.authenticate(trying_to_connect_uid, function(error, facebookID) {
+
+//       if (facebookID) {
+//         console.log("VALID TAG: IT'S GO TIME FOR USERNAME: " + facebookID);
+//         trackRecord.playFavorites(facebookID);
+//         console.log("Streaming: " + trackRecord.isStreaming);
+//         set_last_uid_to_connect(trying_to_connect_uid);
+//       }
+//       else if(error) {
+//         console.log("Error with auth: " + error.message);
+//         return;
+//       } 
+//     });
+//   }
 // });
 
 function set_last_uid_to_connect(uid) {
@@ -76,3 +74,5 @@ function set_last_uid_to_connect(uid) {
 function clear_last_uid() {
     last_uid_to_connect = null;
 }
+trackRecord.connectSpotify(function(spotifySession) { console.log("Succesfully connected to spotify."); trackRecord.playFavorites(fakeUID)});
+// trackRecord.playFavorites(fakeUID);
