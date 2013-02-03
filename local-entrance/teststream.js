f8c34e8b8fdfc71cd2e4d3ca84b974265566ff0c
Paul Booth <thephantompaulbooth@gmail.com>
2013-02-03 11:22:48 -0500

    /stream only does one song

--- local-entrance/teststream.js
+++ local-entrance/teststream.js
@@ -2,12 +2,21 @@ var http = require('http');
 var spawn = require('child_process').spawn;
 
 var play = spawn('play', ['-r', 44100, '-b', 16, '-L', '-c', 2, '-e', 'signed-integer', '-t', 'raw', '-']);
-http.get('http://entranceapp.herokuapp.com/30f911883803c643/stream', function(res) {
-  res.on('data', function(chunk) {
-    console.log(chunk);
+
+function listen() {
+  console.log('listening.');
+  http.get('http://entranceapp.herokuapp.com/30f911883803c643/stream', function(res) {
+    res.on('data', function(chunk) {
+      console.log(chunk);
+    });
+    res.on('error', function(err) {
+      console.log(err);
+    });
+    res.on('end', function() {
+      return listen();
+    });
+    res.pipe(play.stdin);
   });
-  res.on('error', function(err) {
-    console.log(err);
-  })
-  res.pipe(play.stdin);
-});
+}
+
+listen();
