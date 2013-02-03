e0220a13d4268018242ce3eef48ad78d9e9b11b7
Paul Booth <thephantompaulbooth@gmail.com>
2013-02-03 12:59:23 -0500

    fixed error error

--- local-entrance/teststream.js
+++ local-entrance/teststream.js
@@ -1,27 +1,33 @@
 var http = require('http');
 var spawn = require('child_process').spawn;
 
-var play = spawn('play', ['-r', 44100, '-b', 16, '-L', '-c', 2, '-e', 'signed-integer', '-t', 'raw', '-']);
+
 
 function listen() {
+  try{
+  var play = spawn('play', ['-r', 44100, '-b', 16, '-L', '-c', 2, '-e', 'signed-integer', '-t', 'raw', '-']);
   console.log('listening.');
   http.get('http://entranceapp.herokuapp.com/30f911883803c643/stream', function(res) {
     res.on('data', function(chunk) {
       console.log(chunk);
     });
-    res.on('error', function(err) {
-      console.log(err);
-    });
     res.on('end', function() {
+      play.kill();
       return listen();
     });
     res.on('error', function(err) {
       console.log("ERROR");
       console.log(err);
+      play.kill();
       return listen();
     })
     res.pipe(play.stdin);
   });
+  } catch (e) {
+    console.log("serious error.");
+    console.log(e);
+    listen();
+  }
 }
 
 listen();
--- remote-entrance/index.js
+++ remote-entrance/index.js
@@ -70,7 +70,7 @@ function handleTap(localEntranceId, deviceId, hollaback) {
     // or the user isn't in the db and they need to sync
     if (error) {
       console.log("We had an error with Gatekeeper: " + error.message);
-
+      return hollaback({'error': "We had an error with Gatekeeper: " + error.message});
       // Send something to the local entrance device to let it know if
       // a.) there was a network error or b.) the device needs to sync
     } 
@@ -102,7 +102,6 @@ function handleTap(localEntranceId, deviceId, hollaback) {
 
             } else {
               // User left room, but people are still in room
-              // TODO compute new tracks, send them down
               return hollaback({'action' : 'continue', 'message' : 'User removed from session. Reforming track list on server.'});
             }
           });
