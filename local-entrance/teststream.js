c8675aebc0f58b156c9740c95d74d13dca799be7
Paul Booth <thephantompaulbooth@gmail.com>
2013-02-03 00:14:16 -0500

    streaming from the heroku works.

--- local-entrance/teststream.js
+++ local-entrance/teststream.js
@@ -2,7 +2,7 @@ var http = require('http');
 var spawn = require('child_process').spawn;
 
 var play = spawn('play', ['-r', 44100, '-b', 16, '-L', '-c', 2, '-e', 'signed-integer', '-t', 'raw', '-']);
-http.get('http://localhost:3000/testtrackstream', function(res) {
+http.get('http://entranceapp.herokuapp.com/testtrackstream', function(res) {
   res.on('data', function(chunk) {
     console.log(chunk);
   });
