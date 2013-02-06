2f4e077d10f072f3db98b9223284a76f3b97879d
Paul Booth <thephantompaulbooth@gmail.com>
2013-02-06 12:03:20 -0500

    Made teststream have a deviceId that we will need to set somehow

--- local-entrance/teststream.js
+++ local-entrance/teststream.js
@@ -1,11 +1,13 @@
 var http = require('http');
 var spawn = require('child_process').spawn;
 
+var deviceId = '30f911883803c643';
+
 function listen() {
   try{
   var play = spawn('play', ['-r', 44100, '-b', 16, '-L', '-c', 2, '-e', 'signed-integer', '-t', 'raw', '-']);
   console.log('listening.');
-  http.get('http://entranceapp.herokuapp.com/30f911883803c643/stream', function(res) {
+  http.get('http://entranceapp.herokuapp.com/' + deviceId + '/stream', function(res) {
     res.on('data', function(chunk) {
       console.log(chunk);
     });
