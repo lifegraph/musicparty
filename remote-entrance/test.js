d86c60b52cd35e7c30e673b67fd33625c1cf4df5
Tim Cameron Ryan <id@timryan.org>
2013-02-20 09:22:19 -0500

    Test

--- remote-entrance/test.js
+++ remote-entrance/test.js
@@ -4,6 +4,6 @@ var pid = '34567834567';
 
 lifegraph.configure('entrance-tutorial', "481848201872129", "f2696ba2416ae6a4cc9cbde1dddd6a5b");
 
-lifegraph.requestAccess(pid, function (err, json) {
+lifegraph.connect(pid, function (err, json) {
   console.log(err, json);
 })
