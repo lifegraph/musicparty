a8d4329eb8986a6aa8ca01b63c93a1efb6af2263
johnnyman727 <jonathan.mckay@students.olin.edu>
2013-03-14 01:29:10 -0400

    Updated Arduino code to force user to uncomment their hardware

--- arduino_rfid_reader/arduino_rfid_reader.ino
+++ arduino_rfid_reader/arduino_rfid_reader.ino
@@ -1,12 +1,17 @@
-#include <SoftwareSerial.h>
-#include <sm130.h>
-NFCReader nfc(7, 8);
 
+// Uncomment these three lines to use the Sparkfun RFID shield
+// #include <SoftwareSerial.h>
+// #include <sm130.h>
+// NFCReader rfid(7, 8);
+
+// Uncomment these three lines to use the AdaFruit RFID shield
 //#include <Wire.h>
 //#include <Adafruit_NFCShield_I2C.h>
-//Adafruit_NFCShield_I2C nfc(2, 3);
+//Adafruit_NFCShield_I2C rfid(2, 3);
 
+// The number of seconds to wait before accepting another tag
 const uint8_t TIME_DELAY = 2;
+// Var to keep track of time between tags
 long lastReadTime = 0;
 
 
@@ -17,14 +22,14 @@ void setup(void) {
   Serial.begin(9600);
 
   // Start running the RFID shield
-  nfc.begin();
+  rfid.begin();
 
   // Print out a message to make sure Serial is working
   Serial.println("Initialized!");
   Serial.println("Requesting Firmware Version to make sure comm is working...");
 
   // Grab the firmware version
-  uint32_t versiondata = nfc.getFirmwareVersion();
+  uint32_t versiondata = rfid.getFirmwareVersion();
   
   // If nothing is returned, it didn't work. Loop forever
   if (!versiondata) {
@@ -48,7 +53,7 @@ void loop() {
 
   // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
   // 'uid' will be populated with the UID, and uidLength will indicate the length
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  success = rfid.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
 
   // If we succesfully received a tag and it has been greater than the time delay (in seconds)
   if (success &&  (millis() - lastReadTime > (TIME_DELAY * 1000))) {
@@ -59,7 +64,7 @@ void loop() {
     Serial.print(", ID: ");
     
     // Print the ID in hex format
-    nfc.PrintHex(uid, uidLength);
+    rfid.PrintHex(uid, uidLength);
     Serial.println("");
     
     // Same the last read time
