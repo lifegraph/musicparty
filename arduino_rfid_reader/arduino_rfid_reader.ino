11ec42876f0bdfb28189b727dbc74e57b8fc32e6
johnnyman727 <jonathan.mckay@students.olin.edu>
2013-03-14 01:22:20 -0400

    Added arduino code that works for both the sm130 and the AdaFruit shield

--- arduino_rfid_reader/arduino_rfid_reader.ino
+++ arduino_rfid_reader/arduino_rfid_reader.ino
@@ -1,78 +1,70 @@
+#include <SoftwareSerial.h>
+#include <sm130.h>
+NFCReader nfc(7, 8);
 
-#include <Wire.h>
-#include <Adafruit_NFCShield_I2C.h>
+//#include <Wire.h>
+//#include <Adafruit_NFCShield_I2C.h>
+//Adafruit_NFCShield_I2C nfc(2, 3);
 
-#define IRQ   (2)
-#define RESET (3)  // Not connected by default on the NFC Shield
-
-Adafruit_NFCShield_I2C nfc(IRQ, RESET);
+const uint8_t TIME_DELAY = 2;
 long lastReadTime = 0;
 
-void setup(void) {
-  Serial.begin(9600);
-  Serial.println("Hello!");
 
+// Setup method is called once every time Arduino is restarted
+void setup(void) {
+
+  // Start communication with our serial monitor
+  Serial.begin(9600);
+
+  // Start running the RFID shield
   nfc.begin();
 
+  // Print out a message to make sure Serial is working
+  Serial.println("Initialized!");
+  Serial.println("Requesting Firmware Version to make sure comm is working...");
+
+  // Grab the firmware version
   uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata) {
-    Serial.print("Didn't find PN53x board");
-    while (1); // halt
+  
+  // If nothing is returned, it didn't work. Loop forever
+  if (!versiondata) {
+    Serial.print("Didn't find RFID Shield. Check your connection to the Arduino board.");
+    while (1); 
   }
-  // Got ok data, print it out!
-  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
-  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
-  
-  // configure board to read RFID tags
-  nfc.SAMConfig();
-  
-  Serial.println("Waiting for an ISO14443A Card ...");
+ 
+  // Got ok data, good enough!
+  Serial.println("Found Version Data. Comm is working.");
+
+  // Let us know RFID shield is waiting for a tag
+  Serial.println("Waiting for an RFID Card ...");
 }
 
-
-void loop(void) {
+void loop() {
+  
+  // We will store the results of our tag reading in these vars
   uint8_t success;
-  int block_num = 24;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-  uint8_t shouldWrite = false;              // if should write to the fob
+
   // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  // 'uid' will be populated with the UID, and uidLength will indicate the length
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-  
-  if (success && millis() - lastReadTime > 2000) {
-    // Display some basic information about the card
-    Serial.println("Found an ISO14443A card");
-    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
-    Serial.print("  UID Value: ");
+
+  // If we succesfully received a tag and it has been greater than the time delay (in seconds)
+  if (success &&  (millis() - lastReadTime > (TIME_DELAY * 1000))) {
+    Serial.println("Got a tag!");
+    // Print out the length
+    Serial.print("Length: ");
+    Serial.print(uidLength, HEX);
+    Serial.print(", ID: ");
+    
+    // Print the ID in hex format
     nfc.PrintHex(uid, uidLength);
     Serial.println("");
-    lastReadTime = millis();
     
+    // Same the last read time
+    lastReadTime = millis();
   }
 }
 
-
-/**************************************************************************/
-/*! 
-    @brief  Prints a hexadecimal value in plain characters
-
-    @param  data      Pointer to the byte data
-    @param  numBytes  Data length in bytes
-*/
-/**************************************************************************/
-void printHexPlain(const byte * data, const uint32_t numBytes)
-{
-  uint32_t szPos;
-  for (szPos=0; szPos < numBytes; szPos++) 
-  {
-    if (data[szPos] <= 0x1F)
-      Serial.print("");
-    else
-      Serial.print((char)data[szPos]);
-  }
-  Serial.println("");
-}
 
